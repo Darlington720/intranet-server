9f49a8f97ffb9b92072be9f18eacda2beaf79b99
Darlington720 <darlingtonakampa@gmail.com>
2025-03-24 19:08:36 +0300

    added company logo

--- schema/staff/typeDefs.js
+++ schema/staff/typeDefs.js
@@ -3,9 +3,9 @@ const staffTypeDefs = `#graphql
     # staff type is missing field -> email 
     type Staff {
         id: ID!,
-        staff_id: String!,
-        title: String!,
-        staff_name: String!,
+        staff_id: String,
+        title: String,
+        staff_name: String,
         role: String!,
         email: String
     }
